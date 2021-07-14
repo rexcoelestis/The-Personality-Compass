0c5cdd50b9a2d69bfc9c85863b940748da7ccfc5
Rex Mann <rexmann@me.com>
2021-07-13 23:43:49 -0400

    Fixed answer validation and exit from for loop

--- quiz.py
+++ quiz.py
@@ -9,7 +9,9 @@ ready = False
 description = "\nThe description will be here."
 instructions = "\nThe instructions will be here."
 leave = False
-question_set = "'a' or 'b'.  "
+question_set = "'a' or 'b'.  You can always also choose 'q' to quit.  "
+opt1 = "a"
+opt2 = "b"
 
 def calculate_As(list):
     return list.count("a")
@@ -45,25 +47,26 @@ for question in questions:
     questions_seen += 1
     if questions_seen > 30:
         question_set = "'c' or 'd'. You can always also choose 'q' to quit.  "
+        opt1 = "c"
+        opt2 = "d"
     answer = input(question)
     try:
         answer = answer.lower()
-        if questions_seen <= 30:
-            if answer != "a" or answer != "b" or answer != "q":
-                raise ValueError("Did not choose {} ".format(question_set))
-        else:
-            if answer != "a" or answer != "b" or answer != "q":
-                raise ValueError("Did not choose {} ".format(question_set))
+        while answer not in {opt1, opt2, "q"}:
+            raise ValueError("Did not choose {} ".format(question_set))
     except ValueError as err:
         print("Please be sure to select the correct option.  Error: {}".format(err))
     else:
         if answer == "q":
-            exit
+            leave = True
         else:
             answers.append(answer)
 
-print("Thank you for your answers!  ")
-print("Please wait while we calculate your result...  ")
+print("Thank you for your answers!  \n")
+print("Please wait while we calculate your result...  \n")
+print("...")
+print("...")
+print("...")
 
 acount = calculate_As(answers)
 bcount = calculate_Bs(answers)
@@ -84,7 +87,7 @@ p_type_key = ns_type + ew_type
 personality_type = p_types.get(p_type_key)
 type_description = p_type_descr.get(p_type_key)
 
-print("Your personality type is {}.".format(personality_type))
+print("Your personality type is {}. \n".format(personality_type))
 print(type_description)
 print("Thank you for participating!  Have a good one!")
 
